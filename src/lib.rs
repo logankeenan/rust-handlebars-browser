b2aee839136dbe431bd2a2858d4141c9c0859f9d
Logan Keenan <d.logankeenan@gmail.com>
2020-06-04 09:21:03 -0500

    simple example of rendering rust handlebar templates with WASM

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+use wasm_bindgen::prelude::*;
+use handlebars::{Handlebars};
+use std::include_str;
+
+#[macro_use]
+extern crate serde_json;
+
+#[wasm_bindgen]
+pub fn render_template() -> String {
+    let reg = Handlebars::new();
+    let template_string = include_str!("templates/index.hbs");
+
+    let result = reg.render_template(template_string, &json!({"dynamic_text": "foo"}));
+    match result {
+        Ok(template) => {
+            template
+        }
+        Err(_) => {
+            "".to_string()
+        }
+    }
+}
